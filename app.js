54e8d2d1eb2e17121bffe1b6dc1babb690ee3681
Jacob M-G Evans <jacobmgevans@gmail.com>
2017-08-08 16:54:54 -0700

    JS sheet mostly done, HTML bones and text done

---
+++ app.js
@@ -0,0 +1,34 @@
+'use strict';
+var hasDegree = prompt('Do you think I have a college degree?').toLowerCase();
+console.log(hasDegree);
+if (hasDegree === 'y' || 'yes') {alert('That is correct, I have an edumacation');
+}else {alert('BZZZZZZT WRONG!, Move Along.');
+
+}
+var isFather = prompt('Am I a father?').toLowerCase();
+console.log(isFather);
+if (isFather === 'y' || 'yes') {alert('*Darth Vader Voice* Corrrect...');
+
+}else {alert('Luuuke I am a father.');
+
+}
+
+var militaryService = prompt('Did I serve in the U.S. Military?').toLowerCase();
+console.log(militaryService);
+if (militaryService === 'y' || 'yes') {alert('AFFIRM! I served in the Air Force.');
+}else {alert('Big ole Negatory');
+
+}
+var livedHawaii = prompt('Did I ever live in Hawaii?').toLowerCase();
+console.log(livedHawaii);
+if (livedHawaii === 'y' || 'yes') {alert('Aloha! That is correct, I even speak Hawaiian Creole')
+}else {alert('As a matter of fact, I grew up there.')
+
+}
+var challengeAccepted = prompt('From What You Think You Know About Me, Am I Up For The Challenge Of Code Fellows!?').toLowerCase();
+console.log(challengeAccepted);
+if (challengeAccepted === 'y' || 'yes') {
+
+}else {
+
+}
