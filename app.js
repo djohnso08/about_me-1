ab454cf083b726cd72d1c543ad5bdccab3aa5cd7
Jacob M-G Evans <jacobmgevans@gmail.com>
2017-08-08 20:36:44 -0700

    fixed css variable naming convention to kabob, added and removed text from About Me <p> section and some alerts and prompts

--- app.js
+++ app.js
@@ -3,32 +3,28 @@ var hasDegree = prompt('Do You Think I Have A College Degree?').toLowerCase();
 console.log(hasDegree);
 if (hasDegree === 'y' || hasDegree === 'yes') {alert('That Is Correct, I Have An Edumacation');
 }else {alert('BZZZZZZT WRONG!, Move Along.');
-
 }
+
 var isFather = prompt('Am I A Father?').toLowerCase();
 console.log(isFather);
 if (isFather === 'y' || isFather === 'yes') {alert('*Darth Vader Voice* Corrrect...');
-
-}else {alert('Luuuke I Am A Father.');
-
+}else {alert('*Darth Vader Voice* Luuuke I Am A Father... I Do Have A Kid.');
 }
 
 var militaryService = prompt('Did I serve in the U.S. Military?').toLowerCase();
 console.log(militaryService);
 if (militaryService === 'y' || militaryService === 'yes') {alert('AFFIRM! I Served In The Air Force.');
 }else {alert('Big Ole Negatory, HUA!');
-
 }
+
 var livedHawaii = prompt('Did I Ever Live In Hawaii?').toLowerCase();
 console.log(livedHawaii);
 if (livedHawaii === 'y' || livedHawaii === 'yes') {alert('Aloha! That Is Correct, I Even Speak Hawaiian Creole');
 }else {alert('As a matter of fact, I grew up there.');
-
 }
+
 var challengeAccepted = prompt('From What You Think You Know About Me, Am I Up For The Challenge Of Code Fellows!?').toLowerCase();
 console.log(challengeAccepted);
 if (challengeAccepted === 'y' || challengeAccepted === 'yes') {alert('I Think I Can, I Think I Can...I Know I Can.');
-
 }else {alert('Oh Thee Of So Little Faith...');
-
 }
