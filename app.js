09cd53e4f175b9628667046a8508733230a65163
Jacob M-G Evans <jacobmgevans@gmail.com>
2017-08-08 18:27:44 -0700

    Finished JS file, added CSS and HTML

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 'use strict';
 var hasDegree = prompt('Do You Think I Have A College Degree?').toLowerCase();
 console.log(hasDegree);
-if (hasDegree === 'y' || 'yes') {alert('That Is Correct, I Have An Edumacation');
+if (hasDegree === 'y' || hasDegree === 'yes') {alert('That Is Correct, I Have An Edumacation');
 }else {alert('BZZZZZZT WRONG!, Move Along.');
 
 }
 var isFather = prompt('Am I A Father?').toLowerCase();
 console.log(isFather);
-if (isFather === 'y' || 'yes') {alert('*Darth Vader Voice* Corrrect...');
+if (isFather === 'y' || isFather === 'yes') {alert('*Darth Vader Voice* Corrrect...');
 
 }else {alert('Luuuke I Am A Father.');
 
@@ -15,20 +15,20 @@ if (isFather === 'y' || 'yes') {alert('*Darth Vader Voice* Corrrect...');
 
 var militaryService = prompt('Did I serve in the U.S. Military?').toLowerCase();
 console.log(militaryService);
-if (militaryService === 'y' || 'yes') {alert('AFFIRM! I Served In The Air Force.');
+if (militaryService === 'y' || militaryService === 'yes') {alert('AFFIRM! I Served In The Air Force.');
 }else {alert('Big Ole Negatory, HUA!');
 
 }
 var livedHawaii = prompt('Did I Ever Live In Hawaii?').toLowerCase();
 console.log(livedHawaii);
-if (livedHawaii === 'y' || 'yes') {alert('Aloha! That Is Correct, I Even Speak Hawaiian Creole')
-}else {alert('As a matter of fact, I grew up there.')
+if (livedHawaii === 'y' || livedHawaii === 'yes') {alert('Aloha! That Is Correct, I Even Speak Hawaiian Creole');
+}else {alert('As a matter of fact, I grew up there.');
 
 }
 var challengeAccepted = prompt('From What You Think You Know About Me, Am I Up For The Challenge Of Code Fellows!?').toLowerCase();
 console.log(challengeAccepted);
-if (challengeAccepted === 'y' || 'yes') {alert('I Think I Can, I Think I Can...I Know I Can.')
+if (challengeAccepted === 'y' || challengeAccepted === 'yes') {alert('I Think I Can, I Think I Can...I Know I Can.');
 
-}else {alert('Oh Thee Of So Little Faith...')
+}else {alert('Oh Thee Of So Little Faith...');
 
 }
